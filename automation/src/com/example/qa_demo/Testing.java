787546ee1b40392a623127aecc98234e24081eff
Adam Cockell <adam@radiumone.com>
2014-01-08 11:21:39 -0800

    adding src/com file

---
+++ automation/src/com/example/qa_demo/Testing.java
@@ -0,0 +1,32 @@
+package com.example.qa_demo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.RemoteException;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class Testing extends UiAutomatorTestCase {
+
+  public void screenShot(String ofwhat) {
+    long currentTime = System.currentTimeMillis() / 1000L;
+    ofwhat = ofwhat.replaceAll("\\s","");
+    System.out.println("/mnt/sdcard/Pictures/automation/" + currentTime + "_" + ofwhat + ".png");
+    File storePath = new File("/mnt/sdcard/Pictures/automation/" + currentTime + "_" + ofwhat + ".png");
+    getUiDevice().takeScreenshot(storePath);
+  }
+}
