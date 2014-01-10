51f4ece9e16706c6d151042ce59d7a398519a0b0
Adam Cockell <adam@radiumone.com>
2014-01-10 15:46:40 -0800

    cleanup

--- automation/bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/Users/adam/photoeffects/confidentqa/cucumber_ios/automation/bin/classes.dex : \
-/Users/adam/photoeffects/confidentqa/cucumber_ios/automation/bin/classes/com/example/qa_demo/Testing.class \
