baa7325199523c08a2e9394c99a9a92f8e525d1d
Adam Cockell <adam@radiumone.com>
2014-01-15 13:36:40 -0800

    kind of

--- fly_cucumber.rb
+++ fly_cucumber.rb
@@ -238,7 +238,7 @@ class Fly_Cucumber
 
   def save_run_failures results, test_data
     test_data['results']['failures'].push "#{test_data['setup']['feature'].strip} | #{test_data['setup']['scenario_title'].strip} >> #{test_data['setup']['scenario_line'].strip}"
-    if results.length > 1
+    if results.kind_of?(Array)
       test_data['results']['failure_detail'].push results.join(" | ")
     else
       test_data['results']['failure_detail'].push results
