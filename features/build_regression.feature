c7ac7208ac5a9a7d74850c7b85b8205c4e81c5d6
Adam Cockell <adam@radiumone.com>
2014-01-22 14:15:15 -0800

    adding build_regression feature

---
+++ features/build_regression.feature
@@ -0,0 +1,10 @@
+Feature: Build Regression
+  In order to ensure that the app does not crash when upgrading from an older build
+  As someone who is responsible for the app
+  I want to be able to quickly run through the core views
+
+  Scenario: Proceed through views
+    Given I have entered the SDK
+    When I select apply
+    And I select next
+    Then I should see Launch SDK
---
+++ features/step_definitions/build_regression_steps.rb
@@ -0,0 +1,15 @@
+Then("the I should see Launch SDK", function() {
+  expect_visisble(target.frontMostApp().mainWindow().buttons()["Launch SDK"]);
+});
+
+When("I select apply", function(){
+  target.frontMostApp().toolbar().buttons()["Apply"].tap();
+});
+
+When("I select next", function() {
+  target.frontMostApp().mainWindow().buttons()["btn next"].tap();
+});
+
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
