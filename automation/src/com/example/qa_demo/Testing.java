2d43788ea959dc513be71cf1ff53ed7307bd07ef
Adam Cockell <adam@radiumone.com>
2014-02-06 13:54:51 -0800

    adding .trace

--- automation/src/com/example/qa_demo/Testing.java
+++ automation/src/com/example/qa_demo/Testing.java
@@ -33,49 +33,113 @@ Given("I have entered the SDK", function() {
   target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
 
 });
-Then("I should see Launch SDK", function() {
-  expect_visible(target.frontMostApp().mainWindow().buttons()["Launch SDK"]);
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
 });
 
-When("I select apply", function(){
-  target.frontMostApp().toolbar().buttons()["Apply"].tap();
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
 
 });
 
-When("I select next", function() {
-  target.frontMostApp().mainWindow().buttons()["btn next"].tap();
-});
-
-Given("I have entered the SDK", function() {
-  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
-});
-Then("I should see Launch SDK", function() {
-  expect_visible(target.frontMostApp().mainWindow().buttons()["Launch SDK"]);
-});
-
-When("I select apply", function(){
-  target.frontMostApp().toolbar().buttons()["Apply"].tap();
-});
-
-When("I select next", function() {
-  target.frontMostApp().mainWindow().buttons()["btn next"].tap();
-
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
 });
 
 Given("I have entered the SDK", function() {
   target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
 });
-Then("I should see Launch SDK", function() {
-  expect_visible(target.frontMostApp().mainWindow().buttons()["Launch SDK"]);
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
+});
+
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
 
 });
 
-When("I select apply", function(){
-  target.frontMostApp().toolbar().buttons()["Apply"].tap();
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
 });
 
-When("I select next", function() {
-  target.frontMostApp().mainWindow().buttons()["btn next"].tap();
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
+
+});
+
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
+});
+
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
+
+});
+
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
+});
+
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
+
+});
+
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
+});
+
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
+
+});
+
+Given("I have entered the SDK", function() {
+  target.frontMostApp().mainWindow().buttons()["Launch SDK"].tap();
+});
+Then("the (.*) button should not display a down state", function() {
+  expect_equal(Number(target.frontMostApp().mainWindow().buttons()[7].value()),0);
+
+});
+
+When("I select the (.*) option", function(wbutton) {
+  target.frontMostApp().toolbar().buttons()[wbutton].tap();
+});
+
+When("I select effect button (.*) for (.*)", function(buttonVal,buttonName) {
+  target.frontMostApp().mainWindow().buttons()[buttonVal].tap();
 });
 
 Given("I have entered the SDK", function() {
